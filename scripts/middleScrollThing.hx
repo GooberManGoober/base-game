1f12302271310347723a80c538e693ca7d0e279a
GooberManGoober <123832142+GooberManGoober@users.noreply.github.com>
2026-08-02 20:47:50 -0400

    Update middleScrollThing.hx

--- scripts/middleScrollThing.hx
+++ scripts/middleScrollThing.hx
@@ -3,12 +3,8 @@ function onCreatePost()
     if (ClientPrefs.middleScroll)
     {
         modManager.setValue("opponentSwap", 0.5, 0);
-        modManager.setValue("transform0X", -75, 0);
-        modManager.setValue("transform1X", -25, 0);
-        modManager.setValue("transform2X", 25, 0);
-        modManager.setValue("transform3X", 75, 0);
 
-        modManager.setValue("transformX", -650, 1);
+        modManager.setValue("transformX", -600, 1);
         modManager.setValue("transformY", -300, 1);
         modManager.setValue("transformZ", -1, 1);
         modManager.setValue("noteAlpha", 1, 1);
@@ -19,18 +15,12 @@ function onCreatePost()
 
 function onUpdate()
 {
+    dad.vSliceSustains = boyfriend.vSliceSustains = gf.vSliceSustains = true;
+    dad.ghostsEnabled = boyfriend.ghostsEnabled = gf.ghostsEnabled = true;
+    
     if (FlxG.keys.justPressed.NINE)
     {
-        if (iconP1.characterName == boyfriend.healthIcon)
-        {
-            iconP1.changeIcon('bf-old');
-            if (!healthBar.leftToRight) healthBar.setColors(dad.healthColour, CoolUtil.dominantColor(iconP1));
-            else healthBar.setColors(CoolUtil.dominantColor(iconP1), dad.healthColour);
-        }
-        else
-        {
-            iconP1.changeIcon(boyfriend.healthIcon, true);
-            playHUD.reloadHealthBarColors();
-        }
+        if (iconP1.characterName == boyfriend.healthIcon) iconP1.changeIcon('bf-old');
+        else iconP1.changeIcon(boyfriend.healthIcon, true);
     }
 }
